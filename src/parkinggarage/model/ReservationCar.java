5eda06e0a3a537ca4b3a9d5e1bd4411dffd92efe
Mees van Dijk <mees@mees.io>
2019-02-04 04:54:02 +0100

    Add toString to ReservationCar

--- src/parkinggarage/model/ReservationCar.java
+++ src/parkinggarage/model/ReservationCar.java
@@ -52,4 +52,9 @@ public class ReservationCar extends Car {
   public Time getArrivalTime() {
     return arrivalTime;
   }
+  
+  @Override
+  public String toString() {
+	  return super.toString() + " reservationTime: " + time;
+  }
 }
